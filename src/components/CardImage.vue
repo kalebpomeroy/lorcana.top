8e107b8194f2765e2a4a425e77eac7bf7b0d2187
kpom <kpomeroy@mailgun.com>
2024-01-24 09:54:40 -0600

    Play around with buttons

---
+++ src/components/CardImage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div @mouseover="showLargeImage" @mouseleave="hideLargeImage">
+        <img loading="lazy" :src="src"  />
+        <div v-if="showLarge" class="large-image-container" :style="{ top: position.y + 'px', left: position.x + 'px' }">
+            <img :src="src" class="large-image">
+        </div>
+    </div>
+    
+</template>
+           
+<script>
+export default {
+
+    data() {
+        return {
+            showLarge: false,
+            position: {
+                x: 0,
+                y: 0
+            }
+        };
+    },
+    props: {
+        src: String
+    },
+    methods: {
+        showLargeImage(event) {
+            // This needs work to make the overlay not janky
+            // this.showLarge = true;
+            this.position = {
+                x: event.clientX,
+                y: event.clientY
+            }
+        },
+        hideLargeImage() {
+            this.showLarge = false;
+        },
+    }
+};
+</script>
+
+<style>
+.large-image-container {
+
+    position: fixed;
+    z-index: 10;
+    transform: translate(-50%, -50%);
+    width: 250px;
+}
+</style>
